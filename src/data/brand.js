b58c8e3f5d5f650b6b7169f46ea473a3b0269fa5
r3neprod-ops <r3neprod@gmail.com>
2026-02-14 21:16:58 +0300

    style: polish lidar-like ui details and micro-interactions

---
+++ src/data/brand.js
@@ -0,0 +1,9 @@
+export const brand = {
+  name: 'СвойДом Луганск',
+  phoneDisplay: '+7 959 026 0036',
+  phoneHref: '+79590260036',
+  ipLabel: 'ИП: ШЕВЧЕНКО ВЛАДИСЛАВ АНДРЕЕВИЧ',
+  ipInn: 'ИНН: 345969504210',
+  telegramUrl: 'https://t.me/REPLACE_ME',
+  whatsappUrl: 'https://wa.me/79590260036'
+};
